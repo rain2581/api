0f0ff32e68b90fde5494f14f0d601b2d9dd64d27
rain2581 <akonstantin2402@gmail.com>
2018-06-19 15:12:55 +0300

    add python script

---
+++ api/yaml_cfg_formatter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Usage: python yaml_cfg_formatter.py js|firebase source.txt result.yaml """
+
+import sys
+
+
+def main():
+    if sys.argv[1] != 'js' and sys.argv[1] != 'firebase':
+        print('First argument must be "js" or "firebase"')
+        sys.exit(1)
+    result = ['%s\n' % item for item in ('''apiVersion: v1
+data:
+  %s: |+''' % ('cfg.js' if sys.argv[1] == 'js' else 'servolab-sandbox-firebase-adminsdk.json',)).splitlines()]
+
+    with open(sys.argv[2], 'r') as src:
+        result.extend(['    %s' % line for line in src.readlines()])
+
+    result.extend(['%s\n' % item for item in ('''
+kind: ConfigMap
+metadata:
+   name: %s-{{ template "sandbox-api.fullname" . }}
+''' % ('cfg-js' if sys.argv[1] == 'js' else 'firebase-json')).splitlines()])
+
+    with open(sys.argv[3], 'w') as out:
+        out.writelines(result)
+
+
+if __name__ == '__main__':
+    main()
+
